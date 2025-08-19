01a90be806e1171ae61897a84d6f6710bac3f0e5
Victor Escobar <victorescobarhi@gmail.com>
2025-08-18 19:00:22 -0500

    primer push en mi repo

--- demowebflux_130825M/src/main/java/com/victor/demowebflux_130825M/Demowebflux130825MApplication.java
+++ demowebflux_130825M/src/main/java/com/victor/demowebflux_130825M/Demowebflux130825MApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demowebflux130825MApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Demowebflux130825MApplication.class, args);
+		SpringApplication.run(Demowebflux130825MApplication.class, args); //se realiza prueba
 	}
 
 }
